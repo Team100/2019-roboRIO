080224599ce008739903867f0b480e8ebc7a629a
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2019-02-02 10:01:29 -0800

    OfficialRobotBuilder

--- RobotCode/Team100Robot/src/main/java/org/usfirst/frc100/Team100Robot/commands/DriveCMD.java
+++ RobotCode/Team100Robot/src/main/java/org/usfirst/frc100/Team100Robot/commands/DriveCMD.java
@@ -13,6 +13,10 @@ package org.usfirst.frc100.Team100Robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc100.Team100Robot.Robot;
 
+import org.usfirst.frc100.Team100Robot.OI;
+import org.usfirst.frc100.Team100Robot.Robot;
+
+
 /**
  *
  */
@@ -43,6 +47,7 @@ public class DriveCMD extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+        Robot.driveTrain.drive();
     }
 
     // Make this return true when this Command no longer needs to run execute()
